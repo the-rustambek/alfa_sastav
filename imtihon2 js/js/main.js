7c6528efc491cf880a49221052dd90d7a52356db
Rustamjon <80699038+the-rustambek@users.noreply.github.com>
2024-01-21 16:54:46 +0500

    add copy

---
+++ imtihon2 js/js/main.js
@@ -0,0 +1,93 @@
+// 1-masala
+// let n = Number(prompt("sonni kiriting"))
+// if (n>0) {
+
+//     console.log(n**2);
+// }
+// 3-masala
+// let a =prompt("son kirit")
+// if (a>0) {
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+    
+// } else if(a<0) {
+//     console.log(a);
+//     console.log(a);
+//     console.log(a);
+
+    
+// }
+// else{
+//     console.log("son 0");
+// }
+// 10-masala
+// function myapp(a,b) {
+//     if (a+b==100) {
+//         console.log("yuzga teng");
+        
+//     } else {
+//         console.log("yuzga teng emas");
+        
+//     }
+    
+// }
+// myapp(43,55);
+// 9-masala
+// let a =prompt("a ni kirit")
+// let b =prompt("b ni kirit")
+// for (let i=a; i< b; i++) {
+   
+//     console.log(a++);
+
+// }
+// function myapp(a) {
+// if (a>0) {
+//     console.log("a");
+    
+// } else if (a<0) {
+//      for (let i = a; i < 0 ;i+=2) {
+//         console.log(i);
+        
+//     };
+    
+// } 
+// else{
+//     console.log("bu son 0");
+// }
+    
+// }    
+// myapp(-8);
+// 7-masala
+// function myapp(a) {
+//     if (a==366) {
+//         console.log("kabisa yili");
+        
+//     } else {
+//         console.log("kabisa yili emas");
+        
+//     }
+    
+// }
+// myapp(366);
+// 6-masala
+// function myapp(a,b) {
+// if (a-b<10) {
+//     console.log(" farq 10 dan kichik");
+    
+// } else {
+//     console.log("farq 10 dan katta");
+    
+// }    
+// }
+// myapp(34,22);
+// 5-masala
+// function myapp(n) {
+//     for (let i = 0; i < n ;i+=2) {
+//               console.log(i);
+                
+//             };    
+// }
+// myapp(99);
